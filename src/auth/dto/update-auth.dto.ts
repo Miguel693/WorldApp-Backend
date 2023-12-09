7dd71154a3388b8354378c55b1ea3f0e7910f215
Miguel693 <miguelag_2002@hotmail.com>
2023-12-09 01:04:33 -0300

    Dto para editar usuario

--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsString, MinLength, minLength } from 'class-validator';
 
-export class UpdateAuthDto extends PartialType(CreateUserDto) {}
+export class UpdateAuthDto extends PartialType(CreateUserDto) {
+  @IsString()
+  name ?: string;
+  @MinLength(8)
+  password?: string;
+}
