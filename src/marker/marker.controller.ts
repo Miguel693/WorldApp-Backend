36238f2779cf2b01f388f95d856f8c270d1530ab
Miguel693 <miguelag_2002@hotmail.com>
2023-12-10 03:55:19 -0300

    Añadidos los endpoints patch y delete

--- src/marker/marker.controller.ts
+++ src/marker/marker.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
 import { MarkerService } from './marker.service';
-import { CreateMarkerDto } from './dto/create-marker.dto';
+import { UpdateMarkerDto,
+         CreateMarkerDto } from './dto';
 
 @Controller('marker')
 export class MarkerController {
@@ -9,5 +10,16 @@ export class MarkerController {
   @Post('/create')
   create(@Body() createMarkerDto : CreateMarkerDto){
     return this.markerService.create(createMarkerDto);
+  }
+
+  @Delete('del/:id')
+  remove(@Param('id') id: string) {
+    console.log(id);
+    return this.markerService.remove(id);
+  }
+
+  @Patch(':id')
+    update(@Param('id') _id : string, updateMarkerDto : UpdateMarkerDto){
+    return this.markerService.update(_id,updateMarkerDto);
   }
 }
